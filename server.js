88bad0fff769a65798d353e7f4d9405a1cc864fe
Jaap Moerkerk <jaapiemoerkerk@hotmail.com>
2024-02-29 14:04:30 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+import { ChatOpenAI } from "@langchain/openai";
+import express from 'express';
+import dotenv from 'dotenv';
+
+console.log("testing....")
+console.log(process.env.OPENAI_API_KEY)
+
+
+const model = new ChatOpenAI({
+    modelName: "gpt-3.5-turbo",
+    openAIApiKey: process.env.OPENAI_API_KEY
+});
+
+
+const app = express();
+
+app.get('/', async (req, res) => {
+    const joke = await model.invoke("Vertel me een grapje over Sander, hij is erg onzeker over zijn programmeerskills.");
+    res.send(joke.content);
+});
+
+app.listen(3000, () =>{
+    console.log('Server is running on http://localhost:3000');
+});
