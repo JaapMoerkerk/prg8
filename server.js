bdaedb7e8eed23718283f646ebe4f86f3f23d76e
Jaap Moerkerk <jaapiemoerkerk@hotmail.com>
2024-03-06 15:40:38 +0100

    API connected to front end

--- server.js
+++ server.js
@@ -1,26 +1,38 @@
 import { ChatOpenAI } from "@langchain/openai";
 import express from 'express';
 import * as dotenv from "dotenv";
+import cors from 'cors';
+import bodyParser from "body-parser";
+
+const serverPort = 3095;
 
 dotenv.config();
-
-console.log("testing....")
-console.log(process.env.OPENAI_API_KEY)
-
 
 const model = new ChatOpenAI({
     modelName: "gpt-3.5-turbo",
     openAIApiKey: process.env.OPENAI_API_KEY
 });
 
-
 const app = express();
 
-app.get('/', async (req, res) => {
-    const joke = await model.invoke("Vertel me een grapje over Sander, hij is erg onzeker over zijn programmeerskills.");
-    res.send(joke.content);
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.post('/generate', async (req, res) => {
+    try {
+        //Getting the array 'inputList' from the TripTrove call
+        const { inputList } = req.body;
+        // Input list (array of strings) is joined as one string prompt
+        const response = await model.invoke(inputList.join(' '));
+        // Send the response back to the front end
+        res.json({ content: response.content });
+    } catch (error) {
+        console.error("Error processing request:", error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
-app.listen(3000, () =>{
-    console.log('Server is running on http://localhost:3000');
+app.listen(serverPort, () =>{
+    console.log(`Server is running on http://localhost:${serverPort}`);
 });
