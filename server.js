1f5bb5ca30e6e4344f5519850db3210d7f48d43d
Jaap Moerkerk <jaapiemoerkerk@hotmail.com>
2024-03-10 13:11:18 +0100

    API connected to front end

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import { ChatOpenAI } from "@langchain/openai";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
 import express from 'express';
 import * as dotenv from "dotenv";
 import cors from 'cors';
 import bodyParser from "body-parser";
 
 const serverPort = 3095;
+
 
 dotenv.config();
 
@@ -21,10 +23,24 @@ app.use(cors());
 
 app.post('/generate', async (req, res) => {
     try {
-        //Getting the array 'inputList' from the TripTrove call
-        const { inputList } = req.body;
+        //Getting the array 'inputList' from the TripTrove call, joining the list and adding custom instructions.
+        const {inputList} = req.body;
+        const inputListString = inputList.join(', ');
+        const prompt = `
+You are a travel guide, dedicated to recommending the best holiday destination to your clients. Your answers and
+questions are short, concrete and to the point. The user starts the process by giving you (up to) 5 words and/or
+expressions. These terms will be your starting point to figure out what their new ideal holiday destination is.
+Your mission is to respond to that list with 3 short but concrete questions, that will help you narrow down the 
+final answer/destination. Please come up with these questions based on what your first instinct would be after 
+seeing the list. You will ask these question 1 by 1, waiting for the responses. After the last user response you
+will answer with the holiday destination in the format: 'City, Country'.
+This is the list, seperated by comma's: ${inputListString}. Your first response will be the first question out of the 3.
+`;
         // Input list (array of strings) is joined as one string prompt
-        const response = await model.invoke(inputList.join(' '));
+        const response = await model.invoke(
+            prompt
+        );
+        console.log(response);
         // Send the response back to the front end
         res.json({ content: response.content });
     } catch (error) {
